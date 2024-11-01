9147c79a17b16c25ce2c4dd5b31e13283bdfdddc
Raiyan109 <kraiyan109@gmail.com>
2024-11-01 23:22:05 +0600

    4th problem solved

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const onlyNames = onlyFemales.map((n) => n.name)
 console.log(onlyNames)
 
 
+
+
 // 2. Object Manipulation: Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
 const books = [
     {
@@ -49,16 +51,15 @@ const bookTitles = (books) => {
 const bookTitleResult = bookTitles(books)
 console.log(bookTitleResult)
 
-// 3. Function Composition: Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
 
+
+// 3. Function Composition: Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
 function squareNum(num) {
     return num ** 2
 }
-
 function doubleNum(num) {
     return num * 2
 }
-
 function addFive(num) {
     return num + 5
 }
@@ -70,12 +71,34 @@ function functionComposition(function1, function2, function3) {
 }
 
 const squareThenDoubleThenAddFive = functionComposition(squareNum, doubleNum, addFive);
-
 console.log(squareThenDoubleThenAddFive(5))
 
 
 
+// 4. Sorting Objects: Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+const cars = [
+    {
+        name: 'Puzzerro',
+        model: 22,
+        year: 1883
+    },
+    {
+        name: 'Corolla',
+        model: 64,
+        year: 2013
+    },
+    {
+        name: 'Jaguar',
+        model: 92,
+        year: 1818
+    },
+]
 
+function sortCars(cars) {
+    const sortedCars = cars.sort((a, b) => a.year - b.year)
+    return sortedCars
+}
 
+console.log(sortCars(cars))
 
 
